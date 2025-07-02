556b604b7d0f35093f790e7101949622bda180a7
apfp77 <apfp77@naver.com>
2025-07-02 09:18:18 +0900

    feat: 서버 주소 custom axios baseURL로 변경경

--- api/auth.ts
+++ api/auth.ts
@@ -1,5 +1,4 @@
 import apiClient from './apiClient';
-import { getServerURL } from '@/lib/config';
 
 // 로그인 요청 타입
 export interface LoginRequest {
@@ -13,6 +12,6 @@ export interface LoginResponse {
 
 // 로그인 API 요청
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await apiClient.post<LoginResponse>(`${getServerURL()}/users/signin`, data);
+  const response = await apiClient.post<LoginResponse>(`/users/signin`, data);
   return response.data;
 };
