5b4d3c9ab0f53ff10b140914d74bed6c22eb9637
kimyoungjun-dev <kdaon1130@gmail.com>
2025-07-04 16:45:22 +0900

    refactor: 카테고리 정렬코드 제거

--- api/category.ts
+++ api/category.ts
@@ -4,7 +4,7 @@ import { CategoryDto } from '@/types/Category';
 export const fetchCategories = async (): Promise<CategoryDto[]> => {
   try {
     const response = await apiClient.get<CategoryDto[]>('/categories');
-    return response.data.sort((a, b) => a.categoryId - b.categoryId);
+    return response.data;
   } catch (error) {
     console.error('카테고리 정보를 가져오는데 실패했습니다:', error);
     return [];
