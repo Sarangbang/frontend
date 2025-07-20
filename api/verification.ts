9b7f48b9cf703815c32008097356f79bc020ae13
apfp77 <apfp77@naver.com>
2025-07-20 23:12:16 +0900

    feat: 챌린지 인증 사진 추가

--- api/verification.ts
+++ api/verification.ts
@@ -3,29 +3,17 @@ import apiClient from './apiClient';
 import { TodayVerificationStatusResponse, VerificationCreateRequest, VerificationResponse } from '@/types/Verification';
 import toast from 'react-hot-toast';
 
-export const submitVerification = async (formData: FormData) => {
-	console.log(formData);
-  // try {
-  //   // TODO: API 엔드포인트 확정 후 수정 필요
-  //   const response = await apiClient.post('/verifications', formData, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data',
-  //     },
-  //   });
-  //   return response.data;
-  // } catch (error) {
-  //   console.error('인증 제출에 실패했습니다.', error);
-  //   throw error;
-  // }
-};
-
 export const getTodayVerifications = async () : Promise<TodayVerificationStatusResponse[]> => {
   const response = await apiClient.get<TodayVerificationStatusResponse[]>('/challenge-verifications/status');
   return response.data;
 };
 
 export const createChallengeVerification = async (data: VerificationCreateRequest) => {  
-  const response = await apiClient.post<VerificationResponse>('/challenge-verifications', data);
+  const response = await apiClient.post<VerificationResponse>('/challenge-verifications', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
   return response.data;
 };
 
