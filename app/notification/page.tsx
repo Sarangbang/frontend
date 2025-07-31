141d901fb70692f87eaa8e3a9a7b084190e74769
SeongriPark <tjdfl0516@gmail.com>
2025-07-31 10:18:11 +0900

    refactor : 데스크톱 버전 알림 페이지 추가

---
+++ app/notification/page.tsx
@@ -0,0 +1,7 @@
+import NotificationClient from '@/components/notification/NotificationClient';
+
+const NotificationPage = () => {
+  return <NotificationClient />;
+};
+
+export default NotificationPage;
